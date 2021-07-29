06074096ef15cd55287160743682237777e2538c
Joel Gerber <Joel.Gerber@corp.eastlink.ca>
2021-07-29 14:09:01 -0400

    Improve type-hints

--- src/index.js
+++ src/index.js
@@ -1,30 +1,88 @@
 const pino = require("pino");
+const { streamSym } = require("pino/lib/symbols.js");
 
 /**
  * Retrieve a logger.
  * @param {string} layer - software layer to associate logger with
  * @param {string} level - logging level
- * @returns {pino.Logger}
+ * @returns {SimpleLogger}
  */
 const getLogger = (layer, level) => {
-  if (!children.has(layer))
-    children.set(layer, parent.child({ layer }, { level }));
+  if (!children.has(layer)) children.set(layer, new SimpleLogger(layer, level));
   return children.get(layer);
 };
 
 module.exports = { getLogger };
 
-const reformatLevel = (level) => ({ level });
-
-/** @type {Map<string, pino.Logger>} */
+/** @type {Map<string, SimpleLogger>} */
 const children = new Map();
 
-/** @type {pino.Logger} */
-const parent = pino({
-  base: {},
-  formatters: { level: reformatLevel },
-  mixin() {
-    return { msg: undefined };
-  },
-  timestamp: pino.stdTimeFunctions.isoTime,
-});
+class SimpleLogger {
+  /**
+   * Create a new simple logger.
+   * @param {string} layer
+   * @param {string} level
+   */
+  constructor(layer, level) {
+    this.logger = pino({
+      base: {},
+      formatters: { level: (level) => ({ level }) },
+      level,
+      mixin() {
+        return { layer, msg: undefined };
+      },
+      timestamp: pino.stdTimeFunctions.isoTime,
+    });
+    Object.assign(this, { [streamSym]: this.logger[streamSym] });
+  }
+
+  /**
+   * Trace log.
+   * @param {any} object
+   * @param {string} [template]
+   * @param {...any} [parameters]
+   */
+  trace(object, template, ...parameters) {
+    this.logger.trace(object, template, ...parameters);
+  }
+
+  /**
+   * Debug log.
+   * @param {any} object
+   * @param {string} [template]
+   * @param {...any} [parameters]
+   */
+  debug(object, template, ...parameters) {
+    this.logger.debug(object, template, ...parameters);
+  }
+
+  /**
+   * Informative log.
+   * @param {any} object
+   * @param {string} [template]
+   * @param {...any} [parameters]
+   */
+  info(object, template, ...parameters) {
+    this.logger.info(object, template, ...parameters);
+  }
+
+  /**
+   * Warning log.
+   * @param {any} object
+   * @param {string} [template]
+   * @param {...any} [parameters]
+   */
+  warn(object, template, ...parameters) {
+    this.logger.warn(object, template, ...parameters);
+  }
+
+  /**
+   * Error log.
+   * @param {any} object
+   * @param {string} [template]
+   * @param {...any} [parameters]
+   */
+  error(object, template, ...parameters) {
+    this.logger.error(object, template, ...parameters);
+  }
+}
--- src/index.test.js
+++ src/index.test.js
@@ -7,20 +7,23 @@ const { getLogger } = require("./index.js");
 jest.useFakeTimers("modern");
 
 test.each([
-  ["info", "1960-08-09T13:33:19.123Z", "something", {}, "hello world"],
-  ["error", "1954-10-01T08:14:21.234Z", "other", { thing: 1 }, "yo"],
+  ["trace", "1964-03-05T13:43:19.123Z", "1st", { a: "b" }, "hello"],
+  ["debug", "1960-08-25T19:38:10.234Z", "2nd", {}, "bonjour"],
+  ["info", "1959-04-25T00:29:48.345Z", "3rd", { c: { d: 1 } }, "word"],
+  ["warn", "1956-01-02T23:02:39.456Z", "4th", {}, "howdy"],
+  ["error", "1954-10-01T08:14:21.567Z", "5th", { thing: 1 }, "yo"],
 ])(
   "logs when logging level allows for it",
   // eslint-disable-next-line unicorn/prevent-abbreviations
-  (level, time, layer, parameters, msg) => {
+  (level, time, layer, object, msg) => {
     const logger = getLogger(layer, level);
     const stdout = jest.spyOn(logger[streamSym], "write");
     jest.setSystemTime(Date.parse(time));
 
-    logger[level](parameters, msg);
+    logger[level](object, msg);
 
     expect(stdout).toBeCalledWith(
-      JSON.stringify({ level, time, layer, msg, ...parameters }) + "\n"
+      JSON.stringify({ level, time, layer, msg, ...object }) + "\n"
     );
   }
 );
